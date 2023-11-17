94b2acc7ff7fc734e2c80d6c5ee543520233d578
Chethan S <chethans17jan2004@gmail.com>
2023-11-18 02:43:29 +0530

    created span and button

--- script.js
+++ script.js
@@ -5,9 +5,27 @@ let itemList = document.querySelector("#item-list")
 addItem.addEventListener('click', addItemsToList);
 
 function addItemsToList() {
+    //store the inputvalue
+    let inputItem = item.value;
+
+    // clear the input value
+    item.value = "";
+
+    //creates list and append into ul
     let newItem = document.createElement('li')
     newItem.setAttribute("id", "list")
-    // document.getElementById('list').innerHTML = "1st item is";
-    newItem.innerHTML = `value inside the input ${'item'}`
     itemList.appendChild(newItem);
+    //create span and button and append into list
+    let span = document.createElement('span');
+    let deleteButton = document.createElement('button');
+    newItem.appendChild(span)
+    newItem.appendChild(deleteButton)
+
+    span.textContent = inputItem;
+    deleteButton.innerText = 'delet';
+
+    deleteButton.addEventListener('click', function () {
+        itemList.removeChild(newItem);
+    });
+
 }
